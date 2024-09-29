b6fff286e5d01308c54aa1c978a85961f90c262a
Krushna Raut <krishraut2103@gmail.com>
2024-09-29 20:03:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleFAQ(id) {
+   const answer = document.getElementById(`answer${id}`);
+   const icon = document.getElementById(`icon${id}`);
+ 
+   // Toggle display of answer
+   if (answer.style.display === 'block') {
+     answer.style.display = 'none';
+     icon.innerHTML = '<i class="ri-arrow-down-s-line"></i>';
+   } else {
+     answer.style.display = 'block';
+     icon.innerHTML = '<i class="ri-arrow-down-s-line"></i>';
+   }
+ }
+ 
+ document.getElementById('dark-mode-toggle').addEventListener('click', function () {
+   document.body.classList.toggle('dark-mode');
+});
